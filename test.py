4b581a05ca29b9072730dc1a30b2598e7612cdd3
Vladimir Agafonkin <agafonkin@gmail.com>
2014-12-15 15:21:29 +0200

    add initial test

---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+import glob
+import sys
+import json
+
+import encode
+import decode
+
+class DynamicClassBase(unittest.TestCase):
+    longMessage = True
+
+def make_test_function(filename):
+    def test(self):
+        geojson = json.loads(open(filename, 'rb').read())
+        pb = encode.encode(geojson)
+        geojson2 = decode.decode(pb)
+        self.assertEqual(geojson, geojson2, 'encode and decode ' + filename)
+    return test
+
+if __name__ == '__main__':
+
+    for filename in glob.glob("./fixtures/*.json"):
+        klassname = 'Test_' + filename
+        globals()[klassname] = type(klassname,
+            (DynamicClassBase,), {'test_encode_decode': make_test_function(filename)})
+
+    unittest.main()
