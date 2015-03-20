639ab462cd77345df405697fd64fe5d2ca987d2b
Sean Gillies <sean@mapbox.com>
2015-03-20 11:13:19 -0600

    Minor encoding bug fix gets us to 100% coverage.
    Also add: AUTHORS and CHANGES files, a manifest template, and
    conversion of markdown to restructured text for the benefit of
    PyPI.

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 from codecs import open as codecs_open
+import distutils.log
+import os.path
+import shutil
 from setuptools import setup, find_packages
+import subprocess
 
+
+# Try to convert README markdown to restructured text using pandoc.
+try:
+    subprocess.call(
+        'pandoc --from=markdown --to=rst --output=README README.md',
+        shell=True)
+    assert os.path.exists('README')
+except:
+    distutils.log.warn(
+        "Conversion of README.md to restructured text was not successful.")
+    shutil.copy('README.md', 'README')
 
 # Get the long description from the relevant file
-with codecs_open('README.md', encoding='utf-8') as f:
+with codecs_open('README', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pygeobuf',
-      version='1.0',
+      version='1.0.0',
       description=(
         u"Geobuf is a compact binary geospatial format for lossless "
         u"compression of GeoJSON and TopoJSON data."),
