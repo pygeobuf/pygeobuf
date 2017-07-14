36aa04de101f6174d6a44a4dcbddbd92003fab24
Jake Guthmiller <jguthmiller@mindmixer.com>
2017-07-14 13:34:33 -0500

    bump to the next major version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with codecs_open('README', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='geobuf',
-      version='1.1.1',
+      version='2.0.0',
       description=(
           u"Geobuf is a compact binary geospatial format for lossless "
           u"compression of GeoJSON and TopoJSON data."),
